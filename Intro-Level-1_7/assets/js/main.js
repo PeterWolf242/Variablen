720a4166f303d03c40342127b44738c74637f3cb
[PeterWolf242] <[peter.ebm@web.de]>
2024-12-09 15:05:06 +0100

    Variablen

---
+++ Intro-Level-1_7/assets/js/main.js
@@ -0,0 +1,13 @@
+let helloworld = "Hello World";
+
+const pi=3.14;
+
+let burjdubai = "828m";
+
+let fullName = "Jan Schmidt";
+
+let eifelTower = "324m";
+
+let camelcase = "camelCase";
+
+let kursStatus = true;
---
+++ Variablen-Level-1_6/assets/js/main.js
@@ -0,0 +1,13 @@
+// let x = 34;
+
+// x = 67;
+
+// console.log(x);
+
+const y = 34;
+
+y = 34;
+
+console.log(y);
+
+// Der Fehler in der Console taucht auf weil die Variable y als Constante festgelegt wurde und im Nachinein nicht mehr überschrieben werden darf
